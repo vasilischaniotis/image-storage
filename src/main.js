fa4c0d951e56aa800e07cc2f763311ef5754946c
Vasilis Chaniotis <bchaniotis@hotmail.com>
2019-07-08 02:50:45 +0300

    After lecture 100

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
+import AuthHandler from './components/AuthHandler';
+
+Vue.use(VueRouter);
+
+const route = new VueRouter({
+    routes: [
+        { path: '/oauth2/callback', component: AuthHandler }
+    ]
+});
 
 new Vue({
+    router,
     store,
     render: h => h(App)
 }).$mount('#app');
