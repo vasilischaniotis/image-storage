02d25614eac8d645c84c58334cbd60f5a37cf4db
Vasilis Chaniotis <bchaniotis@hotmail.com>
2019-07-08 04:09:42 +0300

    After lecture 114

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import VueRouter from 'vue-router';
 import App from './App';
 import store from './store';
 import AuthHandler from './components/AuthHandler';
+import ImageList from './components/ImageList.vue';
+import UploadForm from './components/UploadForm.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
     mode: 'history',
     routes: [
+        { path: '/', component: ImageList },
+        { path: '/upload', component: UploadForm },
         { path: '/oauth2/callback', component: AuthHandler }
     ]
 });
